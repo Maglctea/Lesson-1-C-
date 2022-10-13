7df190151c790f81d0732105dd0ff2580e425119
Fortunella <sherbakovdaniil6@gmail.com>
2022-10-13 21:47:48 +0300

    Выполнено 2 задания

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1
+// Console.WriteLine("Введите 1 число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+//
+// Console.WriteLine("Введите 2 число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+//
+// if (b * b == a)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// Задание 2.1
+// Console.WriteLine("Введите число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+//
+// if (a < 1 || a > 7) 
+// {
+//     Console.WriteLine("Такого дня нет:");
+// }
+//
+// if (a == 1)
+//     Console.WriteLine("Понедельник");
+// if (a == 2)
+//     Console.WriteLine("Вторник");
+// if (a == 3)
+//     Console.WriteLine("Среда");
+// if (a == 4)
+//     Console.WriteLine("Четверг");
+// if (a == 5)
+//     Console.WriteLine("Пятница");
+// if (a == 6)
+//     Console.WriteLine("Суббота");
+// if (a == 7)
+//     Console.WriteLine("Воскресенье:");
+
+// Задание 2.2
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+    break;
+
+    case 2:
+        Console.WriteLine("Вторник");
+    break;
+
+    case 3:
+        Console.WriteLine("Среда");
+    break;
+
+    case 4:
+        Console.WriteLine("Четверг");
+    break;
+
+    case 5:
+        Console.WriteLine("Пятница");
+    break;
+
+    case 6:
+        Console.WriteLine("Суббота");
+    break;
+
+    case 7:
+        Console.WriteLine("Воскресенье");
+    break;
+
+    default:
+        Console.WriteLine("Такого дня нет");
+    break;
+}
